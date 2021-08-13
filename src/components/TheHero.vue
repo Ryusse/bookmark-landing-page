ea3e0ba1e01363ff783c8c3b864f941adc0072d1
FarisPalayi <fariswritecode@gmail.com>
2021-08-13 06:16:35 +0530

    hero section mobile version prototype complete

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,30 +1,29 @@
 <template>
   <section class="hero">
-    <div class="wrapper">
-      <div class="hero__col hero__col--1">
-        <h1 class="hero__col__title">A Simple Bookmark Manager</h1>
-        <p class="hero__col__desc">
-          A clean and simple interface to organize your favourite websites. Open
-          a new browser tab and see your sites load instantly. Try it for free.
-        </p>
-        <div class="hero__col__btns">
-          <BaseButton>Get it on Chrome</BaseButton>
-          <BaseButton
-            color="hsl(229, 31%, 21%)"
-            bgColor="hsl(180deg 9% 98%)"
-            class="firefox-btn"
-          >
-            Get it on Firefox
-          </BaseButton>
-        </div>
+    <div class="hero__col hero__col--1"></div>
+    <div class="hero__col hero__col--2">
+      <h1 class="hero__col__title">A Simple Bookmark Manager</h1>
+      <p class="hero__col__desc">
+        A clean and simple interface to organize your favourite websites. Open a
+        new browser tab and see your sites load instantly. Try it for free.
+      </p>
+      <div class="hero__col__btns">
+        <BaseButton>Get it on Chrome</BaseButton>
+        <BaseButton
+          color="hsl(229, 31%, 21%)"
+          bgColor="hsl(180deg 9% 98%)"
+          class="firefox-btn"
+        >
+          Get it on Firefox
+        </BaseButton>
       </div>
-      <div class="hero__col hero__col--2"></div>
     </div>
   </section>
 </template>
 
 <script>
-import BaseButton from "./BaseButton.vue"; 
+import BaseButton from "./BaseButton.vue";
+import "../assets/images/illustration-hero.svg";
 
 export default {
   name: "TheHero",
@@ -37,22 +36,54 @@ export default {
 <style lang="scss">
 @use '../assets/sass/abstracts/all' as *;
 
-.wrapper {
-  flex: 1;
-  min-height: calc(100vh - 158px);
-  @include flexbox(column-reverse);
+.hero {
   @include query(md) {
-    @include flexbox(row, null, center);
-    > * { flex:1 1 100% }
+    @include flexbox(row-reverse);
   }
 }
 
 .hero__col--1 {
-  @include flexbox(col, null, center);
-  text-align: center;
+  position: relative;
+  width: 100%;
+  height: 64vw;
+  isolation: isolate;
+  margin-bottom: 2.6vw;
+  // border: 1px solid #000;
   @include query(md) {
+    height: auto;
+  }
+
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+  }
+
+  &::before {
+    width: 85%;
+    height: 84%;
+    right: -2.58%;
+    top: 20%;
+    background-color: $soft-blue;
+    border-radius: 5000px 0 0 5000px;
+  }
+
+  &::after {
+    width: 100%;
+    height: 100%;
+    background: url("../assets/images/illustration-hero.svg") center no-repeat;
+    background-size: 100%;
+  }
+}
+
+.hero__col--2 {
+  margin-top: 18vw;
+  padding: 0 6vw;
+  text-align: center;
+  @include flexbox(col, null, center);
+  @include query(md) {
+    margin-top: 0;
     text-align: left;
-    @include flexbox(col, null, flex-start);
   }
 }
 
@@ -67,9 +98,9 @@ export default {
 .hero__col__desc {
   max-width: 540px;
   margin-top: 20px;
-  @include font-size($p-font-sizes);
   color: $grayish-blue;
   line-height: 1.5;
+  @include font-size($p-font-sizes);
 }
 
 .hero__col__btns {
@@ -82,38 +113,14 @@ export default {
   .firefox-btn > button {
     box-shadow: 0 5px 7px #00000020;
     margin-left: 5px;
+
     &:hover,
     &:focus {
       background-color: transparent !important;
       color: hsl(229, 31%, 21%) !important;
       border-color: hsl(229, 31%, 21%) !important;
     }
-  }
-}
 
-.hero__col--2 {
-  position: relative;
-  background-color: coral;
-  background: url("../assets/images/illustration-hero.svg") no-repeat center;
-  background-size: contain;
-  height: max(350px, 50vw);
-  background-size: max(330px, 70vw);
-  @include query(md) {
-    background-size: contain;
-  }
-
-  &::before {
-    content: "";
-    position: absolute;
-    height: 50%;
-    width: 80%;
-    right: -10%;
-    top: 50%;
-    transform: translateY(-40%);
-    background-color: $soft-blue;
-    border-radius: 5000px 0 0 5000px;
-    overflow: hidden;
-    z-index: -1;
   }
 }
 </style>
