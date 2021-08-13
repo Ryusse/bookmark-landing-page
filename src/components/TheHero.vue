2708f8ff3834090e1f693d049cb42a07978b9dd1
FarisPalayi <fariswritecode@gmail.com>
2021-08-13 08:42:52 +0530

    hero responsiveness halfway done

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,21 +1,23 @@
 <template>
   <section class="hero">
-    <div class="hero__col hero__col--1"></div>
-    <div class="hero__col hero__col--2">
-      <h1 class="hero__col__title">A Simple Bookmark Manager</h1>
-      <p class="hero__col__desc">
-        A clean and simple interface to organize your favourite websites. Open a
-        new browser tab and see your sites load instantly. Try it for free.
-      </p>
-      <div class="hero__col__btns">
-        <BaseButton>Get it on Chrome</BaseButton>
-        <BaseButton
-          color="hsl(229, 31%, 21%)"
-          bgColor="hsl(180deg 9% 98%)"
-          class="firefox-btn"
-        >
-          Get it on Firefox
-        </BaseButton>
+    <div class="wrapper">
+      <div class="hero__col hero__col--illustration"></div>
+      <div class="hero__col hero__col--content">
+        <h1 class="hero__col__title">A Simple Bookmark Manager</h1>
+        <p class="hero__col__desc">
+          A clean and simple interface to organize your favourite websites. Open a
+          new browser tab and see your sites load instantly. Try it for free.
+        </p>
+        <div class="hero__col__btns">
+          <BaseButton>Get it on Chrome</BaseButton>
+          <BaseButton
+            color="hsl(229, 31%, 21%)"
+            bgColor="hsl(180deg 9% 98%)"
+            class="firefox-btn"
+          >
+            Get it on Firefox
+          </BaseButton>
+        </div>
       </div>
     </div>
   </section>
@@ -36,21 +38,24 @@ export default {
 <style lang="scss">
 @use '../assets/sass/abstracts/all' as *;
 
-.hero {
+.wrapper {
   @include query(md) {
     @include flexbox(row-reverse);
+    > * {
+      flex: 1 1 100%;
+    }
   }
 }
 
-.hero__col--1 {
+.hero__col--illustration {
   position: relative;
   width: 100%;
-  height: 64vw;
+  height: 67vw;
   isolation: isolate;
   margin-bottom: 2.6vw;
   // border: 1px solid #000;
   @include query(md) {
-    height: auto;
+    height: 33vw;
   }
 
   &::before,
@@ -60,12 +65,16 @@ export default {
   }
 
   &::before {
-    width: 85%;
+    width: 83%;
     height: 84%;
-    right: -2.58%;
+    right: 0;
     top: 20%;
     background-color: $soft-blue;
     border-radius: 5000px 0 0 5000px;
+
+    @include query(md) {
+      width: 71%;
+    }
   }
 
   &::after {
@@ -76,7 +85,7 @@ export default {
   }
 }
 
-.hero__col--2 {
+.hero__col--content {
   margin-top: 18vw;
   padding: 0 6vw;
   text-align: center;
@@ -84,6 +93,8 @@ export default {
   @include query(md) {
     margin-top: 0;
     text-align: left;
+    padding-right: 0;
+    @include flexbox(col, null, flex-start);
   }
 }
 
@@ -92,7 +103,7 @@ export default {
   font-weight: 500;
   color: $very-dark-blue;
   @include font-size($h1-font-sizes);
-  @include query(md) { max-width: none }
+  @include query(md) { max-width: 500px }
 }
 
 .hero__col__desc {
@@ -108,7 +119,10 @@ export default {
   margin-top: 30px;
   max-width: 380px;
   @include flexbox(null, space-between);
-  @include query(md) { max-width: 345px }
+  @include query(md) {
+    max-width: 345px;
+    @include flexbox(null, flex-start)
+  }
 
   .firefox-btn > button {
     box-shadow: 0 5px 7px #00000020;
@@ -120,7 +134,6 @@ export default {
       color: hsl(229, 31%, 21%) !important;
       border-color: hsl(229, 31%, 21%) !important;
     }
-
   }
 }
 </style>
