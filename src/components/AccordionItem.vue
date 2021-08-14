cdc963f0759b6aa41a3ef61295efc0b8c5fc6181
FarisPalayi <fariswritecode@gmail.com>
2021-08-14 15:43:40 +0530

    feature: accordion toggles when pressed tab key

--- src/components/AccordionItem.vue
+++ src/components/AccordionItem.vue
@@ -1,6 +1,11 @@
 <template>
   <li :class="{ expanded: expanded }">
-    <button class="accordion-question" @click="expanded = !expanded" :aria-expanded="expanded">
+    <button
+      class="accordion-question"
+      @click="toggleQuestion"
+      @keyup.space="toggleQuestion"
+      :aria-expanded="expanded"
+    >
       <span class="accordion-question__text"> {{ question }} </span>
       <span class="accordion-question__arrow-icon">
         <IconArrow />
@@ -20,7 +25,7 @@ import IconArrow from "./icons/IconArrow.vue";
 export default {
   name: "AccordionItem",
   components: {
-    IconArrow
+    IconArrow,
   },
   props: {
     question: {
@@ -36,6 +41,11 @@ export default {
     return {
       expanded: false,
     };
+  },
+  methods: {
+    toggleQuestion() {
+      this.expanded = !this.expanded;
+    },
   },
 };
 </script>
@@ -65,7 +75,7 @@ export default {
   min-width: 50px;
   @extend %grid-center;
   transition: transform 250ms linear;
-  color: #5267DF;
+  color: #5267df;
 }
 
 .expanded .accordion-question__arrow-icon {
