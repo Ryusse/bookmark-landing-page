c685eaf0a1dde32e54c735f0859084178a9a57a0
Ryusse <joelangeltw@gmail.com>
2021-08-13 20:47:55 -0500

    changes from master

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,21 @@
   <main class="layout"> 
     <TheHeader />
     <TheHero />
-    <TheFeatures/>
-    
+    <TheExtension />
   </main>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
 import TheHero from "./components/TheHero.vue";
-import TheFeatures from "./components/TheFeatures.vue";
+import TheExtension from './components/TheExtension.vue';
 
 export default {
   name: "App",
   components: {
     TheHeader,
     TheHero,
-    TheFeatures,
+    TheExtension,
   },
 };
 </script>
