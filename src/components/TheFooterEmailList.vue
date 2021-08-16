49c15c58f3acf7528b333763d6c3ccf89a6ee58e
FarisPalayi <fariswritecode@gmail.com>
2021-08-16 10:16:46 +0530

    footer email validation prototype completed

--- src/components/TheFooterEmailList.vue
+++ src/components/TheFooterEmailList.vue
@@ -2,22 +2,39 @@
   <section class="email-list">
     <span class="email-list__subtitle">35,000+ already joined</span>
     <h2 class="email-list__title">Stay up-to-date with what we’re doing</h2>
-    <form class="email-list__signup">
-      <input
-        type="email"
-        name="emailList"
-        class="email-list__signup__email"
-        placeholder="Enter your email address"
-      />
-      <BaseButton
-        :bgColor="'hsl(0, 94%, 66%)'"
-        :width="125"
-        :height="46"
-        class="email-list__signup__btn"
+    <template v-if="!subscribed">
+      <form
+        class="email-list__signup"
+        @submit.prevent="validateEmail"
+        @keyup.enter="validateEmail"
+        :class="{ invalid: !isEmailValid }"
       >
-        Contact Us
-      </BaseButton>
-    </form>
+        <div class="input-wrapper">
+          <input
+            type="email"
+            name="emailList"
+            class="email-list__signup__email"
+            placeholder="Enter your email address"
+            v-model="userEnteredEmail"
+            required
+          />
+          <transition name="email-alert-transition">
+            <span class="email-list__signup__alert" v-if="!isEmailValid">
+              Whoops, make sure it's an email
+            </span>
+          </transition>
+        </div>
+        <BaseButton
+          :bgColor="'hsl(0, 94%, 66%)'"
+          :width="125"
+          :height="46"
+          class="email-list__signup__btn"
+        >
+          Contact Us
+        </BaseButton>
+      </form>
+    </template>
+    <h3 v-else class="email-list__thank-note">Thanks for subscribing!</h3>
   </section>
 </template>
 
@@ -29,12 +46,29 @@ export default {
   components: {
     BaseButton,
   },
-  emialRegex: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+  data() {
+    return {
+      userEnteredEmail: "",
+      isEmailValid: true,
+      subscribed: false,
+    };
+  },
   methods: {
-    validateEmail(email) {
+    validateEmail() {
       const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return pattern.test(String(email).toLowerCase());
+      const isValid = pattern.test(String(this.userEnteredEmail).toLowerCase());
+      this.isEmailValid = isValid;
+      this.subscribed = isValid;
     },
   },
 };
 </script>
+
+<style lang="scss">
+.invalid .input-wrapper::after {
+  content: url("../assets/images/icon-error.svg");
+  position: absolute;
+  right: 10px;
+  transform: translateY(50%);
+}
+</style>
