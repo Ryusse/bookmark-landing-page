bd0c55116db14db895be2b595019e249d45e43ed
FarisPalayi <fariswritecode@gmail.com>
2021-08-18 20:31:31 +0530

    add basic `:focus` state styles.

--- src/components/Footer/TheFooterNav.vue
+++ src/components/Footer/TheFooterNav.vue
@@ -1,9 +1,11 @@
 <template>
   <aside class="footer__aside">
     <div class="footer__aside__container">
-      <a href="#" class="footer__aside__logo">
-        <img src="../../assets/images/logo-bookmark-white.svg" alt="bookmark" />
-      </a>
+      <span class="footer__aside__logo-wrapper">
+        <a href="#" class="footer__aside__logo">
+          <img src="../../assets/images/logo-bookmark-white.svg" alt="bookmark" />
+        </a>
+      </span>
       <nav class="footer__nav">
         <ul class="footer__nav__ul">
           <li class="footer__nav__ul__li" v-for="item in navList" :key="item">
